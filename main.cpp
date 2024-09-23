a9f968d8cff4b93a420a22c0c5e7d1a442e2eec2
KaelTheVoker <mrfoxy3099@gmail.com>
2024-09-23 14:36:16 +0200

    lab-2 is ready

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using std::cout;
+using std::endl;
+using std::cin;
+using std::sqrt;
+
+int main(){
+    // variant 1 
+    int a;
+    int b;
+    int c;
+    cout << "Input a, b and c" << endl;
+    cin >> a; cin >> b; cin >> c;
+
+    float p = (a + b + c)/2;
+    cout << p << endl;
+    float area = sqrt(p * (p - a) * (p - b) * (p - c));
+
+    cout << "Area of a triangle: " + std::to_string(area) << endl;
+
+    return 0;
+}
+
